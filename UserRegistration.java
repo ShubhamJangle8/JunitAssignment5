a4aaf830dc7cba4003d850ed2d672a7652289ec9
jangle <shubhamjangale0000@gmail.com>
2020-10-02 22:19:46 +0530

    Refactored code to Validate First Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,5 +4,22 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration Portal");
+		System.out.println("Welcome to User Registration Portal");  
+		Scanner sc = new Scanner(System.in);
+		UserRegistration user = new UserRegistration();
+		System.out.println("Enter the first name");
+		String firstName = sc.nextLine();
+		System.out.println(firstName + " is " +user.validateInput(firstName));
+		
+		
+	}
+	private String validateInput(String name) {
+		String expression = "(^[A-Z]{1})[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(expression);
+		Matcher match = pattern.matcher(name);
+		if(match.find()) {
+			return "valid";
+		}
+		return "invalid";
+	}
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -1,2 +1,8 @@
 class UserRegistrationDetailsTest {
+	@Test
+	void FNametest() {
+		UserRegistrationDetails obj = new UserRegistrationDetails();
+		String actual = obj.validateFName("Shubham");
+		Assert.assertEquals("valid",actual);	
+	}
 }
