c51a2f2a22a7b9964bec377fe80502864940b6ef
jangle <shubhamjangale0000@gmail.com>
2020-10-02 23:08:18 +0530

    Refactored code to Validate Password consisting of Minimum 8 Characters minimum with atleast 1 UpperCase and 1 Numeric Value with exactly 1 Special Character

--- UserRegistration.java
+++ UserRegistration.java
@@ -56,7 +56,7 @@ public class UserRegistration {
 	}
 
 	private String validatePassword(String pass) {
-		String expression = "^((?=.*[A-Z]+)(?=.*[0-9]+)([A-Za-z0-9@#$%^&*]{8,}))$";	//Pattern for Password
+		String expression = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@!%_&])[A-Za-z0-9$#@!%_&]{8,}$";	//Pattern for Password
 		Pattern pattern = Pattern.compile(expression);
 		Matcher match = pattern.matcher(pass);
 		if(match.find()) {
